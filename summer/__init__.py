978230b8c7e920794ddb3fa59ecc334e9900f103
Jia Chunying <iccccing@gmail.com>
2018-12-21 22:36:27 +0800

    2018.12.21 v0.0.2  #24 modify import method, add MANIFEST.in file

--- summer/__init__.py
+++ summer/__init__.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from summer.app import app, Base, Summer, Blueprint
+from summer.app import *
 from summer.command import cli
 
 engine = create_engine(app.config.get('SQLALCHEMY_DATABASE_URI'),
@@ -8,4 +8,4 @@ engine = create_engine(app.config.get('SQLALCHEMY_DATABASE_URI'),
 Session = sessionmaker(bind=engine)
 db = Session()
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
